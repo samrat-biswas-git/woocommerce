ed522a031f8dc2baca2999481500ab5b0c6170a5
Tung Du <dinhtungdu@gmail.com>
2026-05-04 18:42:19 +0700

    fix: dynamic filter style

--- plugins/woocommerce/client/blocks/assets/js/blocks/product-filters/frontend.ts
+++ plugins/woocommerce/client/blocks/assets/js/blocks/product-filters/frontend.ts
@@ -7,6 +7,7 @@ import * as iAPI from '@wordpress/interactivity';
  * Internal dependencies
  */
 import { decodeHtmlEntities } from '../../utils/html-entities';
+import setStyles from './set-styles';
 
 const { getContext, store, getServerContext, getConfig } = iAPI;
 
@@ -246,6 +247,8 @@ const productFiltersStore = {
 };
 
 export type ProductFiltersStore = typeof productFiltersStore;
+
+setStyles();
 
 const { state, actions } = store< ProductFiltersStore >(
 	BLOCK_NAME,
